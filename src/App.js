9945d0dcab52ad5074147df45a5a33d6550077d1
ZSchleger <zachs376@gmail.com>
2023-11-16 16:57:23 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import './App.css';
 import { useState } from 'react';
+import { sculptureList } from './data.js';
 
-export default function MyApp() {
-  return (
-      <div>
-        <h1>Counters that update separately</h1>
-        <MyButton />
-        <MyButton />
-      </div>
-  );
-}
+export default function Gallery() {
+    const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
+    const hasNext = index < sculptureList.length - 1;
 
-function MyButton() {
-  const [count, setCount] = useState(0);
+    function handleNextClick() {
+        if (hasNext) {
+            setIndex(index + 1);
+        } else {
+            setIndex(0);
+        }
+    }
 
-  function handleClick() {
-    setCount(count + 1);
-  }
+    function handleMoreClick() {
+        setShowMore(!showMore);
+    }
 
-  return (
-      <button onClick={handleClick}>
-        Clicked {count} times
-      </button>
-  );
+    let sculpture = sculptureList[index];
+    return (
+        <>
+            <button onClick={handleNextClick}>
+                Next
+            </button>
+            <h2>
+                <i>{sculpture.name} </i>
+                by {sculpture.artist}
+            </h2>
+            <h3>
+                ({index + 1} of {sculptureList.length})
+            </h3>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            {showMore && <p>{sculpture.description}</p>}
+            <img
+                src={sculpture.url}
+                alt={sculpture.alt}
+            />
+        </>
+    );
 }
