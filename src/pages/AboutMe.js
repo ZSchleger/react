944ae0904e5bcd3a3623a1247cd2f35905bea34b
ZSchleger <zachs376@gmail.com>
2023-12-14 00:05:45 -0500

    Updates

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const AboutMe = () => {
     return (
@@ -6,6 +7,10 @@ const AboutMe = () => {
             <h1>About Me</h1>
             <h2>Zach Schleger</h2>
             <p>I am currently a 4th year student at Penn State Behrend majoring in Digital Media Arts and Technology</p>
+
+            <Link to="https://github.com/ZSchleger">
+                <button>GitHub</button>
+            </Link>
         </div>
     );
 }
